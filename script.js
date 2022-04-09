f5366f51fc62df722c786962aedc3c7cbbd9d2f2
@budy6991 <alvmenfer@gmail.com>
2022-04-09 22:43:34 +0300

    Added clear button

--- script.js
+++ script.js
@@ -1,13 +1,12 @@
+//Creates the functions for the operations
+
 function add(x,y){
     return x + y
 }
 
-
 function substract (x,y){
     return x - y
 }
-
-
 
 function multiply (x,y){
     return x*y
@@ -16,6 +15,9 @@ function multiply (x,y){
 function divide (x,y){
     return x/y
 }
+
+//Creates the function based on the operator selected
+
 
 function operator(operator, num1, num2) {
 
@@ -36,3 +38,28 @@ function operator(operator, num1, num2) {
     }
 
 }
+
+// Creating the event listeners 
+
+const btns = document.querySelectorAll('button:not(.clear, .equal');
+const display = document.querySelector('.display')
+const btnClear = document.querySelector('.clear')
+const btnEqual = document.querySelector('.equal')
+
+btns.forEach(btn => {
+
+
+    btn.addEventListener('click', (e)=> {
+      const store = e.target.textContent
+      return display.textContent += store
+        
+    });
+
+ });
+
+
+btnClear.addEventListener('click', (e) => {
+   const store = e.target.textContent
+   display.textContent = ""
+})
+
