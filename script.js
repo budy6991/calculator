865a02636f0e187240b33ecd31a42d1824d38141
@budy6991 <alvmenfer@gmail.com>
2022-04-12 15:35:37 +0300

    Renamed variables

--- script.js
+++ script.js
@@ -46,84 +46,51 @@ const display = document.querySelector('.display')
 const btnClear = document.querySelector('.clear')
 const btnEqual = document.querySelector('.equal')
 
-let store = 0;
-let storeTwo = 0;
-let storeThree =0;
-let operation = '';
-let pairOfNumbers = 0;
-let result = 0;
+    let firstInput = 0;
+    let secondInput = 0;
+    let operation = '';
 
 btns.forEach(btn => {
     
-    
-
     btn.addEventListener('click', (e)=> {
         
-        
-
     if (Number(e.target.textContent) <= 9 && operation == '') {
-    store = Number(e.target.textContent)
-    display.textContent += store
-    storeTwo = Number(display.textContent)
+        firstInput = Number(e.target.textContent)
+        display.textContent += firstInput
+        firstInput = Number(display.textContent)
     }
     
     else if (e.target.textContent == '+' || e.target.textContent == '-' || e.target.textContent == 'x' || e.target.textContent == '/' ){
         display.textContent = ''
         operation = e.target.textContent
         
-        
     }
 
     else if (Number(e.target.textContent) <= 9) {
         
-        store = Number(e.target.textContent)
-        display.textContent += store
-        storeThree = Number(display.textContent)
+        secondInput = Number(e.target.textContent)
+        display.textContent += secondInput
+        secondInput = Number(display.textContent)
         
         }
     
     
     else if (e.target.textContent == '='){
-      display.textContent = (operator(`${operation}`, storeTwo, storeThree))
-        storeTwo = (operator(`${operation}`, storeTwo, storeThree))
-        pairOfNumbers = storeTwo
-        console.log(pairOfNumbers)
+      display.textContent = (operator(`${operation}`, firstInput, secondInput))
+        secondInput = (operator(`${operation}`, firstInput, secondInput))
         
     }
-
-    else if (e.target.textContent == '+' || e.target.textContent == '-' || e.target.textContent == 'x' || e.target.textContent == '/' ){
-        display.textContent = pairOfNumbers
-        operation = e.target.textContent
-    }
-
-    else if (Number(e.target.textContent) <= 9) {
-        
-        store = pairOfNumbers
-        display.textContent += store
-        result = Number(display.textContent)
-        
-        }
-        
+    
     else if (e.target.textContent == 'AC'){
-        store = 0;
-        storeTwo = 0;
-        storeThree =0;
+        firstInput = 0;
+        secondInput = 0;
         operation = '';
         display.textContent = ""
     }
 
-    
-
     });
     
  });
-
-
-//The calculator should not evaluate more than a single pair of numbers at a time 
-
-
-
-
 
 
 
